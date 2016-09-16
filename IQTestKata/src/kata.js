0664c606ee81d6e637e98b6a541924db0e212401
wellwind <wellwind@mail2000.com.tw>
2016-09-16 15:27:53 +0800

    second green light

--- IQTestKata/src/kata.js
+++ IQTestKata/src/kata.js
@@ -1,5 +1,20 @@
 "use strict";
 function kata(numbers) {
-    return 3;
+    var odds = [];
+    var evens = [];
+    numbers.split(" ").forEach(function (num, index) {
+        if (parseInt(num) % 2 == 1) {
+            odds.push(index);
+        }
+        else {
+            evens.push(index);
+        }
+    });
+    if (odds.length == 1) {
+        return odds[0] + 1;
+    }
+    else {
+        return evens[0] + 1;
+    }
 }
 exports.kata = kata;
--- IQTestKata/src/kata.ts
+++ IQTestKata/src/kata.ts
@@ -1,3 +1,17 @@
 export function kata(numbers: string): number {
-    return 3;
+    let odds: Array<number> = [];
+    let evens: Array<number> = [];
+    numbers.split(" ").forEach(function(num: string, index: number) {
+        if(parseInt(num) % 2 == 1) {// odds
+            odds.push(index);
+        } else {
+            evens.push(index);
+        }
+    });
+
+    if (odds.length == 1) {
+        return odds[0] + 1;
+    } else {
+        return evens[0] + 1;
+    }
 }
