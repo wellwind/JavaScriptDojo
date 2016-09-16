ced1105bd9fa288b6adf7644efadfe7eefde95a9
wellwind <wellwind@mail2000.com.tw>
2016-09-16 15:22:03 +0800

    second red light

--- IQTestKata/test/kata-test.js
+++ IQTestKata/test/kata-test.js
@@ -8,4 +8,7 @@ describe('kata test', function () {
     it('should return 3 when input is "2 4 7 8 10"', function () {
         chai_1.expect(kata_1.kata('2 4 7 8 10')).to.eql(3);
     });
+    it('should return 1 when input is "1 2 2"', function () {
+        chai_1.expect(kata_1.kata('1 2 2')).to.eql(1);
+    });
 });
--- IQTestKata/test/kata-test.ts
+++ IQTestKata/test/kata-test.ts
@@ -9,4 +9,8 @@ describe('kata test', () => {
     it('should return 3 when input is "2 4 7 8 10"', () => {
         expect(kata('2 4 7 8 10')).to.eql(3);
     });
+
+    it('should return 1 when input is "1 2 2"', () => {
+        expect(kata('1 2 2')).to.eql(1);
+    });
 });
