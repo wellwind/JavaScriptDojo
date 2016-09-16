87a5d92c8d8d006217e4416c55123d37976c2689
wellwind <wellwind@mail2000.com.tw>
2016-09-16 15:17:52 +0800

    Init Project

---
+++ IQTestKata/test/kata-test.js
@@ -0,0 +1,7 @@
+"use strict";
+var chai_1 = require("chai");
+describe('kata test', function () {
+    it('should pass this canary test', function () {
+        chai_1.expect(true).to.eql(true);
+    });
+});
---
+++ IQTestKata/test/kata-test.ts
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import { kata } from '../src/kata';
+
+describe('kata test', () => {
+    it('should pass this canary test', () => {
+        expect(true).to.eql(true);
+    });
+});
