3d68ceb5139ff41a5d0b36d9d8aa64226ff264a7
wellwind <wellwind@mail2000.com.tw>
2016-09-17 14:04:23 +0800

    Finish NaN test

--- SumStringsAsNumbersKata/src/kata.ts
+++ SumStringsAsNumbersKata/src/kata.ts
@@ -1,9 +1,32 @@
 export function kata(val1: string, val2: string): string {
-    let v1 = parseInt(val1);
-    let v2 = parseInt(val2);
+    let result = "";
 
-    if(isNaN(v1)) v1 = 0;
-    if(isNaN(v2)) v2 = 0;
+    let tmpVal1 = val1.split("").reverse().join("");
+    let tmpVal2 = val2.split("").reverse().join("");
+    let next = 0;
+    for(let i = 0; i < (tmpVal1.length > tmpVal2.length ? tmpVal1.length : tmpVal2.length); ++i){
+        let substrA = tmpVal1.substr(i, 1);
+        let substrB = tmpVal2.substr(i, 1);
 
-    return (v1 + v2).toString();
+        let valA = (substrA === "" ? 0 : parseInt(substrA));
+        let valB = (substrB === "" ? 0 : parseInt(substrB));
+        
+
+        let tmpResult = valA + valB + next;
+
+        if(tmpResult >= 10){
+            tmpResult -= 10;
+            next = 1;
+        }else{
+            next = 0;
+        }
+
+        result = tmpResult + result;
+    }
+
+    if(next === 1){
+        result = "1" + result;
+    }
+
+    return result;
 }
--- SumStringsAsNumbersKata/test/kata-test.ts
+++ SumStringsAsNumbersKata/test/kata-test.ts
@@ -13,4 +13,9 @@ describe('kata test', () => {
     it('should return "5" whne inputs are "" & "5"', () => {
         expect(kata("", "5")).to.eql("5", "NaN should treat as 0");
     });
+
+    it('should return "712577413488402631964821329" whne inputs are "712569312664357328695151392" & "8100824045303269669937"', () => {
+        expect(kata("712569312664357328695151392", "8100824045303269669937")).
+            to.eql("712577413488402631964821329", "big integer");
+    });
 });
