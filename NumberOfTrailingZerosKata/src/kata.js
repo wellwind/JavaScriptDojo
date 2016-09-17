2714c5ab7c1d651d7a0cfe185dcd232776cae367
wellwind <wellwind@mail2000.com.tw>
2016-09-17 17:06:07 +0800

    Remove useless comment

--- NumberOfTrailingZerosKata/src/kata.js
+++ NumberOfTrailingZerosKata/src/kata.js
@@ -1,23 +1,5 @@
 "use strict";
 function kata(n) {
-    // let count = 0;
-    // let tmp = 1;
-    // for (let i = 1; i <= n; ++i) {
-    //     tmp *= i;
-    //     if (tmp % 10 === 0) {
-    //         while (tmp % 10 === 0) { // count number of zero trailing
-    //             tmp /= 10;
-    //             ++count;
-    //         }
-    //     } else {
-    //         // keep only last digit
-    //         tmp = tmp % 10;
-    //     }
-    // }
-    // var n = Math.floor(n / 2);
-    // if(n == 0){
-    //     return 0; 
-    // }
     var base = 5;
     var result = 0;
     while (base < n) {
